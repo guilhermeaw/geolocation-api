fcf012fafc03ca0df8e51d83f7c6382b3a824339
Guilherme Weissheimer <guilherme.w@universo.univates.br>
2023-07-03 21:37:21 -0300

    Ordena rotas

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(errors);
 
 app.get('/routes', async (req, res) => {
-  const routes = await prisma.route.findMany();
+  const routes = await prisma.route.findMany({ orderBy: { createdAt: 'desc' } });
   res.json(routes);
 });
 
